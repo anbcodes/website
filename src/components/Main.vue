6709c7a8c18024d2857509a2b797453fd3e5f63d
anbcodes <anbcodes@protonmail.com>
2019-09-20 15:03:30 -0400

    changed CNAME

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -32,46 +32,46 @@ export default {
         name: "School Planner",
         description: "A weekly planner for CC",
         color: "orange",
-        link: "School_Planner"
+        link: "schoolplanner"
       },
       {
         name: "Lesson Planner",
         description: "A lesson planner for CC",
         color: "indigo",
-        link: "Lesson_Planner"
+        link: "lessonplanner"
       },
       {
         name: "Multiplication tables",
         description: "A game where you can review mulitplication facts",
         color: "blue",
-        link: "MultiplicationTables"
+        link: "timestables"
       },
       {
         name: "Latin Vocabulary Review",
         description:
           "A game that asks which weeks you want to review then creates random questions with multiple choice answers",
         color: "purple",
-        link: "LatinVocabReview"
+        link: "latinreview"
       },
       {
         name: "Latin Charts",
         description:
           "A app where you fill in the other side of a chart with latin or english words",
         color: "dark-blue",
-        link: "LatinCharts"
+        link: "latincharts"
       },
       {
         name: "Latin Board",
         description:
           "Latin Board generates a 6x6 grid of latin vocabulary which you can use to review vocabulary",
         color: "deep-purple",
-        link: "LatinBoard"
+        link: "latinboard"
       }
     ]
   }),
   methods: {
     website(value) {
-      return  "/" + value;
+      return value + ".usefulwebapps.com";
     }
   }
 };
